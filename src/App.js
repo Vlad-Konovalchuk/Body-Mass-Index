d2469f332f9ef7dd6708b5df5d533838a973ed43
Vlad Konovalchuk <berlitio600@gmail.com>
2018-12-02 22:41:53 +0200

    Add output for result data

--- src/App.js
+++ src/App.js
@@ -28,13 +28,14 @@ setBmi () {
     console.log(this.state)
   })
 }
+
 setBmiClass(bmi){
   if(bmi < 18.5 ) return "Underweight";
   if(bmi >= 18.5 && bmi <= 24.9 ) return "Normal";
   if(bmi >= 25 && bmi <= 29.9 ) return "Overweight";
   if(bmi >= 30) return "Obese";
-
 }
+
   render(){
     const {height,weight} = this.state;
 
@@ -52,7 +53,7 @@ setBmiClass(bmi){
         </div>
       </form>
       <br/><br/>
-      <Output/>
+      <Output data={this.state}/>
       </div>
     );
   }
