b23a8d2f98a3343ece49384374a06c3aaebb279c
Vlad Konovalchuk <berlitio600@gmail.com>
2018-12-03 00:14:29 +0200

    Updates

---
+++ precache-manifest.af78053270dbf46e745cf9e920ed291b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec08a58dbc34dd41743",
+    "url": "/Body-Mass-Index/static/js/runtime~main.aec08a58.js"
+  },
+  {
+    "revision": "1ffb76cabb1b0a473f5f",
+    "url": "/Body-Mass-Index/static/js/main.1ffb76ca.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/Body-Mass-Index/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "1ffb76cabb1b0a473f5f",
+    "url": "/Body-Mass-Index/static/css/main.b0b09fc1.chunk.css"
+  },
+  {
+    "revision": "6ff17389176b4a42f079e8629c61445e",
+    "url": "/Body-Mass-Index/index.html"
+  }
+];
